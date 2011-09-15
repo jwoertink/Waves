081fb8a1abbf20302786453ec9148090fff6d201
Jeremy Woertink <jeremywoertink@gmail.com>
2011-09-15 11:31:48 -0700

    oops. it works now

--- lib/waves.rb
+++ lib/waves.rb
@@ -1,9 +1,9 @@
 require 'java'
 
 PROJECT_ROOT = File.expand_path('..', File.dirname(__FILE__))
-COMMON_DIR = File.join(File.expand_path('..', File.dirname(__FILE__)), 'vendor', 'Common')
+COMMON_DIR = File.join(PROJECT_ROOT, 'vendor', 'Common')
 
-require "#{PROJECT_ROOT}/vendor/jme3_2011-08-29.jar"
+require File.join(PROJECT_ROOT, 'vendor', 'jme3_2011-08-29.jar')
 
 java_import "com.jme3.app.SimpleApplication"
 java_import "com.jme3.material.Material"
@@ -15,9 +15,6 @@ java_import "com.jme3.math.ColorRGBA"
 class Waves < SimpleApplication
   
   def simpleInitApp
-
-    # The typical "Hello World" jme code, ported straight from:
-    # http://jmonkeyengine.org/wiki/doku.php/jme3:beginner:hello_simpleapplication
     box = Box.new(Vector3f::ZERO, 1, 1, 1)
     geometry = Geometry.new("Box", box)
     material = Material.new(self.asset_manager, File.join('Common', 'MatDefs', 'Misc', 'Unshaded.j3md'))
