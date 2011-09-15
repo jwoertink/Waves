641e6e773c65a6835f58025950341f76f7f39494
Jeremy Woertink <jeremywoertink@gmail.com>
2011-09-15 11:58:02 -0700

    added a second box onto the scene graph

--- lib/waves.rb
+++ lib/waves.rb
@@ -11,16 +11,28 @@ java_import "com.jme3.math.Vector3f"
 java_import "com.jme3.scene.Geometry"
 java_import "com.jme3.scene.shape.Box"
 java_import "com.jme3.math.ColorRGBA"
+java_import "com.jme3.scene.Node"
 
 class Waves < SimpleApplication
   
   def simpleInitApp
-    box = Box.new(Vector3f::ZERO, 1, 1, 1)
-    geometry = Geometry.new("Box", box)
-    material = Material.new(self.asset_manager, File.join('Common', 'MatDefs', 'Misc', 'Unshaded.j3md'))
-    material.set_color("Color", ColorRGBA::Red)
-    geometry.material = material
-    self.root_node.attach_child(geometry)
+    box1 = Box.new(Vector3f.new(1,-1,1), 1, 1, 1)
+    blue = Geometry.new("Box", box1)
+    material1 = Material.new(self.asset_manager, File.join('Common', 'MatDefs', 'Misc', 'Unshaded.j3md'))
+    material1.set_color("Color", ColorRGBA::Blue)
+    blue.material = material1
+    
+    box2 = Box.new(Vector3f.new(1,3,1), 1, 1, 1)
+    red = Geometry.new("Box", box2)
+    material2 = Material.new(self.asset_manager, File.join('Common', 'MatDefs', 'Misc', 'Unshaded.j3md'))
+    material2.set_color("Color", ColorRGBA::Red)
+    red.material = material2
+    
+    pivot = Node.new("pivot")
+    self.root_node.attach_child(pivot)
+    pivot.attach_child(blue)
+    pivot.attach_child(red)
+    pivot.rotate(0.4, 0.4, 0.0)
   end
   
 end
