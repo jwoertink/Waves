d3b33363fccca339a2d01dc6bcef98fea45f8c81
Jeremy Woertink <jeremywoertink@gmail.com>
2014-04-03 09:17:42 -0700

    updated readme and a few small things

--- lib/waves.rb
+++ lib/waves.rb
@@ -5,8 +5,6 @@ PROJECT_ROOT = File.expand_path('..', File.dirname(__FILE__))
 $CLASSPATH << File.join(PROJECT_ROOT, "package", "classes", "java")
 $CLASSPATH << File.join(PROJECT_ROOT, "vendor", "jme3-2014-3-31.jar")
 
-#$: << File.join(PROJECT_ROOT, "lib")
-
 require File.join(PROJECT_ROOT, 'vendor', 'jme3-2014-3-31.jar')
 
 module Waves
